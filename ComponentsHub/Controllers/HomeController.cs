6d28d07b243526b56d470ba75e74bd1c0c0b7dca
nickharin <kharinnick@gmail.com>
2023-06-09 00:34:59 +0500

    add test post request

--- ComponentsHub/Controllers/HomeController.cs
+++ ComponentsHub/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using ComponentsHub.Models;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ComponentsHub.Controllers
@@ -18,8 +21,35 @@ namespace ComponentsHub.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            using (HttpClient client = new HttpClient())
+            {
+                // TODO Move to separate class or file(json??)
+                string url = "https://office.promelec.ru/php/ajax-goods-price.php";
+                string requestData = "Item_ID=139728&ajax=true";
+
+                try
+                {
+                    HttpContent content = new StringContent(requestData, Encoding.UTF8, "application/x-www-form-urlencoded");
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    HtmlDocument htmlDoc = new HtmlDocument();
+                    htmlDoc.LoadHtml(responseContent);
+
+                    //TODO parse html
+
+                    //Pass the parsed HTML to the view
+                    ViewBag.ParsedHtml = htmlDoc.DocumentNode.OuterHtml;
+                    //ViewBag.responseContent = responseContent;
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ParsedHtml = "Error: " + ex.Message;
+                }
+            }
             return View();
         }
 
